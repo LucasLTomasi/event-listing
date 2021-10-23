0df9baa8b4a5f1eb07707549164c119706aaa85e
Lucas Tomasi <lucasltomasi@gmail.com>
2021-10-23 07:48:51 -0300

    Add an UI test.

--- EventListingUITests/EventListingUITests.swift
+++ EventListingUITests/EventListingUITests.swift
@@ -1,42 +1,29 @@
-//
-//  EventListingUITests.swift
-//  EventListingUITests
-//
-//  Created by Lucas Tomasi on 22/10/21.
-//
-
 import XCTest
 
 class EventListingUITests: XCTestCase {
-
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testVariousBehaviors() {
         let app = XCUIApplication()
         app.launch()
 
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        // Static texts
+        let eventsListViewTitle = app.staticTexts["Listagem de Eventos"]
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        // Idle
+        XCTAssert(eventsListViewTitle.exists)
+
+        // Select an event
+        app.tables.element(boundBy: 0).cells.element(boundBy: 0).tap()
+        Thread.sleep(forTimeInterval: 1)
+
+        // Go back
+        app.navigationBars["Detalhes"].buttons["Voltar"].tap()
+        Thread.sleep(forTimeInterval: 1)
+
+        // Select another event
+        app.tables.element(boundBy: 0).cells.element(boundBy: 1).tap()
     }
 }
